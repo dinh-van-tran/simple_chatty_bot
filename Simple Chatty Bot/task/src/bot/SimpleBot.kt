2afdecb69140588612495b3c59f8c7a2354c0c2e
Dinh Tran <tranvan538@gmail.com>
2022-12-01 20:12:03 +0700

    Finish stage 6

--- Simple Chatty Bot/task/src/bot/SimpleBot.kt
+++ Simple Chatty Bot/task/src/bot/SimpleBot.kt
@@ -1,28 +1,62 @@
 package bot
 
-
 fun main() {
-    println("Hello! My name is Aid.")
-    println("I was created in 2020.")
+    greet("Aid", "2020")
+    remindName()
+    guessAge()
+    count()
+    test()
+    end()
+}
+
+fun greet(assistantName: String, birthYear: String) {
+    println("Hello! My name is ${assistantName}.")
+    println("I was created in ${birthYear}.")
     println("Please, remind me your name.")
+}
 
+fun remindName() {
     val name = readln()
+    println("What a great name you have, ${name}!")
+}
 
-    println("What a great name you have, $name!")
+fun guessAge() {
     println("Let me guess your age.")
     println("Enter remainders of dividing your age by 3, 5 and 7.")
-
     val remainder3 = readln().toInt()
     val remainder5 = readln().toInt()
     val remainder7 = readln().toInt()
     val age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+    println("Your age is ${age}; that's a good time to start programming!")
+}
 
-    println("Your age is $age; that's a good time to start programming!")
+fun count() {
     println("Now I will prove to you that I can count to any number you want.")
-    val countNumber = readln().toInt()
-    for (number in countNumber downTo 0) {
-        println("${countNumber - number}!")
+    val number = readln().toInt()
+    for (i in 0..number) {
+        print(i)
+        println("!")
     }
+}
 
-    println("Completed, have a nice day!")
+fun test() {
+    println("Let's test your programming knowledge.")
+    println("Why do we use methods?")
+    println("1. To repeat a statement multiple times.")
+    println("2. To decompose a program into several small subroutines.")
+    println("3. To determine the execution time of a program.")
+    println("4. To interrupt the execution of a program.")
+
+    while (true) {
+        val answer = readln()
+        if (answer == "4") {
+            break
+        }
+
+        println("Please, try again.")
+    }
+}
+
+fun end() {
+    println("Congratulations, have a nice day!") // Do not change this text
 }
