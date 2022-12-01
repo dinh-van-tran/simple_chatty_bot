57dfd6c736934a688fcc9de722e0a4d11420974b
Dinh Tran <tranvan538@gmail.com>
2022-12-01 18:06:59 +0700

    Finish stage 2

--- Simple Chatty Bot/task/src/bot/SimpleBot.kt
+++ Simple Chatty Bot/task/src/bot/SimpleBot.kt
@@ -3,4 +3,9 @@ package bot
 fun main() {
     println("Hello! My name is Aid.")
     println("I was created in 2020.")
+    println("Please, remind me your name.")
+
+    val yourName = readln()
+
+    println("What a great name you have, $yourName!")
 }
