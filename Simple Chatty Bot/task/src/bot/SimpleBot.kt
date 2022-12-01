2748fe5fd2edb7929b294459058506c62cfdf2d8
Dinh Tran <tranvan538@gmail.com>
2022-12-01 18:18:28 +0700

    Finish stage 3

--- Simple Chatty Bot/task/src/bot/SimpleBot.kt
+++ Simple Chatty Bot/task/src/bot/SimpleBot.kt
@@ -1,11 +1,22 @@
 package bot
 
+
 fun main() {
     println("Hello! My name is Aid.")
     println("I was created in 2020.")
     println("Please, remind me your name.")
 
-    val yourName = readln()
+    val name = readln()
 
-    println("What a great name you have, $yourName!")
+    println("What a great name you have, $name!")
+    println("Let me guess your age.")
+    println("Enter remainders of dividing your age by 3, 5 and 7.")
+
+    val remainder3 = readln().toInt()
+    val remainder5 = readln().toInt()
+    val remainder7 = readln().toInt()
+
+    val yourAge = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+
+    println("Your age is $yourAge; that's a good time to start programming!")
 }
